827edb780e333e9f03da914f9448eb926371aa45
andrelambert <rm99148@fiap.com.br>
2023-04-11 21:01:41 -0300

    aula de funções 11-04

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //exemplo de funções
+const divElement = document.getElementById("div-box")
+
+//divElement.addEventListener("click", ()=>{
+//    console.log(divElement);
+//});
+
+divElement.addEventListener("click", mostraElemento);
+
+function mostraElemento(params) {
+    console.log(divElement)
+}
+
+function soma() {
+    let n1 = 10
+    let n2 = 5
+    console.log(n1 + n2)
+}
+
+//soma()
